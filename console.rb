fe18f729fe8b50ae6f3cb16b3ce3bf24b3dce14c
Kate Preston <ktweeden@googlemail.com>
2018-02-14 14:05:28 +0000

    save methods for album and artist

---
+++ console.rb
@@ -0,0 +1,14 @@
+require('pry-byebug')
+require_relative('models/album.rb')
+require_relative('models/artist.rb')
+
+
+
+artist1 = Artist.new({'name' => 'Miley Cyrus'})
+artist1.save()
+
+album1 = Album.new({'title' => 'Time of our Lives', 'genre' => 'pop', 'artist_id' => artist1.id})
+album1.save()
+
+binding.pry
+nil
